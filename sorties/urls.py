b90fe31a7843a84a8f6ff6c3ed9f12b161575ec2
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-03-07 22:13:45 +0100

    Debugging: New api endpoint failing to showup

--- sorties/urls.py
+++ sorties/urls.py
@@ -8,7 +8,7 @@ router = DefaultRouter()
 # 2. Register your ViewSets
 # The router automatically builds the 'currency-status' and 'readiness-report' URLs
 router.register(r'aircraft', AircraftViewSet)
-router.register(r'pilots', PilotViewSet)
+router.register(r'pilots', PilotViewSet, basename='pilot')
 router.register(r'sorties', SortieViewSet)
 
 # 3. Wire up the URL patterns
